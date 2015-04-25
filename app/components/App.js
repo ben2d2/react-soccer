70b786518934df8e11901d2797a8b78c49efadc3
ben2d2 <ben2d2@gmail.com>
2015-04-24 20:28:41 -0500

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+
+});
+
+module.exports = App;
