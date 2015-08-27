1e60f580f77f5793fffa9ee6992e0ce49c98c531
ben2d2 <ben2d2@gmail.com>
2015-08-26 21:56:01 -0500

    Edit whitespace

--- app/components/App.js
+++ app/components/App.js
@@ -3,13 +3,11 @@ var React = require('react');
 var Schedule = require('./Schedule');
 
 var App = React.createClass({
-
   render: function() {
     return (
       <Schedule url='games.json' pollInterval={2000}/>
     );
   }
-
 });
 
 module.exports = App;
