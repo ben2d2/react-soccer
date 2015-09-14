a7e68b33b085ee2461030e13c73254dfd262e3b1
ben2d2 <ben2d2@gmail.com>
2015-09-13 21:34:25 -0500

    Crank up the pollInterval

--- app/components/App.js
+++ app/components/App.js
@@ -16,7 +16,7 @@ var App = React.createClass({
         <div>
           <a className="return-to-all" href="/">Return to all</a>
           <h2 className="team-name"><span className="team-color" style={{backgroundColor: team.color}}></span>{team.name}</h2>
-          <Schedule url='games.json' teamId={id ? id[1] : ""}  teamGroup={group ? group[1] : ""}/>
+          <Schedule url='games.json' teamId={id ? id[1] : ""}  teamGroup={group ? group[1] : ""} pollInterval={100000000}/>
         </div>
       );
     } else {
